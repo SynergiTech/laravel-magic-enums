855c725c0e1c79a2b6b1e3eb8c4605c33d624af3
eden chazard <neon.fortytwo@gmail.com>
2025-09-03 11:00:52 +0100

    fake it till you make it

--- tests/Commands/GenerateCommandTest.php
+++ tests/Commands/GenerateCommandTest.php
@@ -2,14 +2,17 @@
 
 namespace SynergiTech\MagicEnums\Tests\Commands;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Testing\PendingCommand;
 use SynergiTech\MagicEnums\Tests\TestCase;
 
 class GenerateCommandTest extends TestCase
 {
     public function testCommandExports(): void
     {
+        Storage::fake();
         $result = $this->artisan('laravel-magic-enums:generate --input=app/Enums --output=enums');
-        $this->assertInstanceOf(\Illuminate\Testing\PendingCommand::class, $result);
+        $this->assertInstanceOf(PendingCommand::class, $result);
         $result->assertSuccessful();
         $this->assertFileExists('enums/index.js');
     }
