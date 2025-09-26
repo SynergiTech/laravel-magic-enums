1548dd066b4ac9d32a70a95488bfed698a177cd9
John Dent <john.dent@synergitech.co.uk>
2025-09-26 10:46:08 +0100

    tinker with the tests

--- tests/Commands/GenerateCommandTest.php
+++ tests/Commands/GenerateCommandTest.php
@@ -2,18 +2,18 @@
 
 namespace SynergiTech\MagicEnums\Tests\Commands;
 
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Testing\PendingCommand;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use SynergiTech\MagicEnums\Tests\TestCase;
 
 class GenerateCommandTest extends TestCase
 {
     public function testCommandExports(): void
     {
-        Storage::fake();
-        $result = $this->artisan('laravel-magic-enums:generate --input=app/Enums --output=enums');
-        $this->assertInstanceOf(PendingCommand::class, $result);
-        $result->assertSuccessful();
-        $this->assertFileExists('./storage/enums/index.js');
+        $command = $this->artisan('laravel-magic-enums:generate --input=app/Enums --output=enums');
+        $this->assertInstanceOf(PendingCommand::class, $command);
+        $result = $command->run();
+        $this->assertSame(SymfonyCommand::SUCCESS, $result);
+        $this->assertFileExists('./enums/index.js');
     }
 }
