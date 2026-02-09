407123f8e778173decb8d82c1ee9dde17cad8a0c
John Dent <john.dent@synergitech.co.uk>
2026-02-09 17:49:40 +0000

    standards

--- extension.php
+++ extension.php
@@ -1,16 +1,16 @@
 <?php
 
 if (interface_exists(\PHPStan\Reflection\ClassConstantReflection::class)) {
-  $extensionClass = \SynergiTech\MagicEnums\PHPStan\MagicEnumConstantExtension::class;
+    $extensionClass = \SynergiTech\MagicEnums\PHPStan\MagicEnumConstantExtension::class;
 } else {
-  $extensionClass = \SynergiTech\MagicEnums\PHPStan\MagicEnumConstantExtensionVersionOne::class;
+    $extensionClass = \SynergiTech\MagicEnums\PHPStan\MagicEnumConstantExtensionVersionOne::class;
 }
 
 return [
-  'services' => [
-    [
-      'class' => $extensionClass,
-      'tags' => ['phpstan.alwaysUsedClassConstantsExtension']
-    ]
-  ]
+    'services' => [
+        [
+            'class' => $extensionClass,
+            'tags' => ['phpstan.alwaysUsedClassConstantsExtension'],
+        ],
+    ],
 ];
